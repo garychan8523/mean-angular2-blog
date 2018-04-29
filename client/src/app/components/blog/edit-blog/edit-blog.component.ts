1883615e8fc9c32fc4a5732c58ee12f7e3b747d8
garychanchihang <garychan8523@gmail.com>
2018-04-29 12:44:18 +0800

    reactive form update

--- client/src/app/components/blog/edit-blog/edit-blog.component.ts
+++ client/src/app/components/blog/edit-blog/edit-blog.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Location } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 import { BlogService } from '../../../services/blog.service';
 
 @Component({
@@ -13,19 +15,51 @@ export class EditBlogComponent implements OnInit {
   message;
   messageClass;
   blog;
-  processing = false;
+  form;
+  processing = true;
   currentUrl;
   loading = true;
 
   constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
   	private location: Location,
   	private activatedRoute: ActivatedRoute,
   	private blogService: BlogService,
   	private router: Router
-  ) { }
+  ) { 
+    this.createEditBlogForm();
+  }
+
+  createEditBlogForm() {
+    this.form = this.formBuilder.group({
+      title: [{value: '', disabled: true}, Validators.compose([
+        Validators.required,
+        Validators.maxLength(100),
+        Validators.minLength(2),
+        this.alphaNumericValidation
+        ])],
+      body: [{value: '', disabled: true}, Validators.compose([
+        Validators.required,
+        Validators.maxLength(1500),
+        Validators.minLength(2)
+        ])]
+    });
+  }
+
+  alphaNumericValidation(controls) {
+    const regExp = new RegExp(/^[a-zA-Z0-9 ]+$/);
+    if(regExp.test(controls.value) || controls.value == '') {
+      return null;
+    }else {
+      return { 'alphaNumericValidation': true }
+    }
+  }
 
   updateBlogSubmit() {
   	this.processing = true;
+    this.form.controls['title'].disable();
+    this.form.controls['body'].disable();
   	let regex2 = /\n/g
   	this.blog.body = this.blog.body.replace(regex2, "<br>");
   	this.blogService.editBlog(this.blog).subscribe(data => {
@@ -33,6 +67,8 @@ export class EditBlogComponent implements OnInit {
   			this.messageClass = 'alert alert-danger';
         	this.message = data.message;
         	this.processing = false;
+          this.form.controls['title'].enable();
+          this.form.controls['body'].enable();
   		} else {
   			this.messageClass = 'alert alert-success';
   			this.message = data.message;
@@ -48,19 +84,27 @@ export class EditBlogComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.authService.getProfile().subscribe(profile => {
+      if(!profile.success){
+        this.authService.logout();
+        window.location.reload();
+      }
+    });
+
   	this.currentUrl = this.activatedRoute.snapshot.params;
-  	this.blogService.getSingleBlog(this.currentUrl.id).subscribe(data => {
-  		if (!data.success) {
-  			this.messageClass = 'alert alert-danger';
-        	this.message = data.message;
-  		} else {
-  			console.log('data.blog.body: ' + data.blog.body);
-        	let regex = /<br\s*[\/]?>/gi;
-        	data.blog.body = data.blog.body.replace(regex, "\n");
-      		this.blog = data.blog;
-  			this.loading = false;
-  		}
-  		
+    this.blogService.getSingleBlog(this.currentUrl.id).subscribe(data => {
+    	if (!data.success) {
+    		this.messageClass = 'alert alert-danger';
+        this.message = data.message;
+    	} else {
+        let regex = /<br\s*[\/]?>/gi;
+        data.blog.body = data.blog.body.replace(regex, "\n");
+       	this.blog = data.blog;
+        this.processing = false;
+        this.form.controls['title'].enable();
+        this.form.controls['body'].enable();
+    		this.loading = false;
+    	}
   	});
   }
 
