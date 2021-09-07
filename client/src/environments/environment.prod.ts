12210bb7119a80ff7c33d945448b9f6993e541f5
garychanchihang <garychan8523@gmail.com>
2021-09-07 22:07:23 +0800

    update api endpoint

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-54-95-130-37.ap-northeast-1.compute.amazonaws.com/'
+  // apiUrl: 'http://ec2-54-95-130-37.ap-northeast-1.compute.amazonaws.com/'
+  apiUrl: 'https://api.dedd.ca/'
 };
