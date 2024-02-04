34e8a507360284f6210bfa142f783d869f1fed81
garychanchihang <garychan8523@gmail.com>
2024-02-04 14:50:37 +0800

    add new for ObjectId

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 const Blog = require('../models/blog');
 const User = require('../models/user');
